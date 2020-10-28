87b89f001ab34020996dc703e43ee76fb2833d1d
Amit Seker <aseker00@gmail.com>
2020-10-29 00:22:05 +0200

    Support RNN layers in token decoder. Add optimization scheduler.

--- model.py
+++ model.py
@@ -5,18 +5,19 @@ import torch
 
 class TokenSeq2SeqMorphSeg(nn.Module):
 
-    def __init__(self, char_emb, enc_hidden_size, enc_num_layers, enc_dropout, dec_num_layers, dec_dropout, out_size):
+    def __init__(self, char_emb, hidden_size, num_layers, enc_dropout, dec_dropout, out_size, out_dropout):
         super().__init__()
         self.char_emb = char_emb
         self.enc_input_size = self.char_emb.embedding_dim
-        self.enc_hidden_size = enc_hidden_size
-        self.enc_num_layers = enc_num_layers
+        self.enc_hidden_size = hidden_size
+        self.enc_num_layers = num_layers
         self.enc_dropout = enc_dropout
         self.dec_input_size = self.char_emb.embedding_dim
-        self.dec_hidden_size = self.enc_hidden_size
-        self.dec_num_layers = dec_num_layers
+        self.dec_hidden_size = hidden_size
+        self.dec_num_layers = num_layers
         self.dec_dropout = dec_dropout
         self.out_size = out_size
+        self.out_dropput = out_dropout
         self.encoder = nn.GRU(input_size=self.enc_input_size,
                               hidden_size=self.enc_hidden_size,
                               num_layers=self.enc_num_layers,
@@ -30,24 +31,31 @@ class TokenSeq2SeqMorphSeg(nn.Module):
                               batch_first=False,
                               dropout=self.dec_dropout)
         self.out = nn.Linear(in_features=self.dec_hidden_size, out_features=self.out_size)
+        self.dropout = nn.Dropout(self.out_dropput)
 
-    def forward(self, in_token_char_seq, in_enc_state, out_char_seq, sos_char, eos_char,
-                use_teacher_forcing):
+    def forward(self, in_token_char_seq, in_token_state, sos, eos, max_len, target_char_seq):
         emb_chars = self.char_emb(in_token_char_seq).unsqueeze(1)
-        enc_output, dec_state = self.encoder(emb_chars, in_enc_state)
-        dec_char = sos_char
+
+        # hidden = [n layers * n directions, batch size, hidden dim]
+        enc_state = torch.split(in_token_state, in_token_state.shape[2] // self.enc_num_layers, dim=2)
+        enc_state = torch.cat(enc_state, dim=0)
+        # enc_state = tuple(torch.split(hs, hs.shape[2] // self.enc_num_layers, dim=2) for hs in in_token_state)
+        # enc_state = tuple(torch.cat(hs, dim=0) for hs in enc_state)
+
+        enc_output, dec_state = self.encoder(emb_chars, enc_state)
+        dec_char = sos
         dec_char_scores = []
-        max_len = out_char_seq.shape[1]
         while len(dec_char_scores) < max_len:
             emb_dec_char = self.char_emb(dec_char).unsqueeze(1)
             dec_output, dec_state = self.decoder(emb_dec_char, dec_state)
+            dec_output = self.dropout(dec_output)
             dec_output = self.out(dec_output)
-            if use_teacher_forcing:
-                dec_char = out_char_seq[:, len(dec_char_scores)]
+            if target_char_seq is not None:
+                dec_char = target_char_seq[:, len(dec_char_scores)]
             else:
                 dec_char = self.decode(dec_output).squeeze(0)
             dec_char_scores.append(dec_output)
-            if torch.all(torch.eq(dec_char, eos_char)):
+            if torch.all(torch.eq(dec_char, eos)):
                 break
         fill_len = max_len - len(dec_char_scores)
         dec_char_scores.extend([dec_char_scores[-1]] * fill_len)
@@ -59,17 +67,15 @@ class TokenSeq2SeqMorphSeg(nn.Module):
 
 class MorphSegModel(nn.Module):
 
-    def __init__(self, xmodel, xtokenizer, char_emb, char_vocab, token_morph_seg):
+    def __init__(self, xmodel, xtokenizer, char_emb, token_morph_seg):
         super().__init__()
         self.xmodel = xmodel
         self.xtokenizer = xtokenizer
         self.char_emb = char_emb
-        self.char_vocab = char_vocab
         self.token_morph_seg = token_morph_seg
-        self.loss_fct = nn.CrossEntropyLoss(reduction='mean', ignore_index=self.char_vocab['char2index']['<pad>'])
 
-    def forward(self, xtokens, tokens, form_chars, max_tokens, max_form_len, sos_char, eos_char, use_teacher_forcing):
-        dec_scores = []
+    def forward(self, xtokens, tokens, sos, eos, max_form_len, target_form_chars):
+        scores = []
         mask = xtokens != self.xtokenizer.pad_token_id
         token_ctx, sent_ctx = self.xmodel(xtokens, attention_mask=mask)
         cur_token_id = 1
@@ -77,23 +83,25 @@ class MorphSegModel(nn.Module):
         while token_chars.nelement() > 0:
             xtoken_ids = token_chars[:, 1]
             token_state = torch.mean(token_ctx[:, xtoken_ids], dim=1).unsqueeze(1)
-            token_form_chars = form_chars[:, (cur_token_id-1)*max_form_len:cur_token_id*max_form_len]
-            char_seq = token_chars[:, 2]
-            dec_token_scores = self.token_morph_seg(char_seq, token_state, token_form_chars, sos_char, eos_char,
-                                                    use_teacher_forcing)
-            dec_scores.append(dec_token_scores)
+            # token_state = (torch.mean(token_ctx[:, xtoken_ids], dim=1).unsqueeze(1), sent_ctx.unsqueeze(1))
+            input_chars = token_chars[:, 2]
+            target_chars = None
+            if target_form_chars is not None:
+                target_chars = target_form_chars[:, (cur_token_id - 1) * max_form_len:cur_token_id * max_form_len]
+            morph_scores = self.token_morph_seg(input_chars, token_state, sos, eos, max_form_len, target_chars)
+            scores.append(morph_scores)
             cur_token_id += 1
             token_chars = tokens[tokens[:, :, 0] == cur_token_id]
         num_tokens = cur_token_id - 1
-        fill_len = max_tokens - num_tokens
-        dec_scores.extend([dec_scores[-1]] * fill_len)
-        return num_tokens, torch.cat(dec_scores, dim=1)
+        # fill_len = max_tokens - num_tokens
+        # dec_scores.extend([dec_scores[-1]] * fill_len)
+        return num_tokens, torch.cat(scores, dim=1)
         # dec_scores.extend([dec_scores[-1]] * fill_len)
         # dec_scores = torch.cat(dec_scores, dim=1)
         # return num_tokens, F.pad(input=dec_scores, pad=(0, 0, 0, fill_len, 0, 0), mode='constant', value=0)
 
-    def loss(self, dec_scores, gold_chars):
-        return self.loss_fct(dec_scores[0], gold_chars[0])
+    # def loss(self, dec_scores, gold_chars):
+    #     return self.loss_fct(dec_scores[0], gold_chars[0])
 
     def decode(self, label_scores):
         return torch.argmax(label_scores, dim=2)
--- morph_seg_train.py
+++ morph_seg_train.py
@@ -5,12 +5,12 @@ import torch.nn as nn
 from torch.optim import AdamW
 from torch.utils.data import DataLoader, TensorDataset
 from tqdm import trange
-from transformers import AutoTokenizer, AutoModel
-# get_linear_schedule_with_warmup
+from transformers import AutoTokenizer, AutoModel, get_linear_schedule_with_warmup
 import dataset
 from bclm import treebank as tb
 from pathlib import Path
 from model import MorphSegModel, TokenSeq2SeqMorphSeg
+# from sklearn.metrics import accuracy_score, f1_score
 
 
 # Logging setup
@@ -23,7 +23,7 @@ logging.basicConfig(
 
 
 def get_raw_data(annotated_tensor_data, char_vocab):
-    seg_data = []
+    target_data = []
     for i, batch in enumerate(annotated_tensor_data):
         xtoken_data, token_char_data, form_char_data, token_form_char_data = batch
         sent_ids = token_char_data[:, :, 0]
@@ -31,10 +31,10 @@ def get_raw_data(annotated_tensor_data, char_vocab):
         token_form_chars = token_form_char_data[:, :, 1]
         num_tokens = token_char_data[:, :, 1].max().item()
         max_form_len = token_form_char_data[:, :, 3].unique().item()
-        gold_chars = decode(token_form_chars, num_tokens, max_form_len, char_vocab)
-        gold_form_chars = to_token_chars(gold_chars, char_vocab)
-        seg_data.append((sent_ids.numpy(), token_chars.numpy(), gold_form_chars.numpy()))
-    return dataset.to_raw_data(seg_data, char_vocab)
+        target_chars = pack_padded_labels(token_form_chars, num_tokens, max_form_len, char_vocab)
+        target_form_chars = to_token_chars(target_chars, char_vocab)
+        target_data.append((sent_ids.numpy(), token_chars.numpy(), target_form_chars.numpy()))
+    return dataset.to_raw_data(target_data, char_vocab)
 
 
 def get_tensor_dataset(data_path, partition, xtokenizer, char_vocab):
@@ -87,29 +87,20 @@ train_batch_size = 1
 train_dataloader = DataLoader(dataset_partition['train'], batch_size=train_batch_size, shuffle=False)
 dev_dataloader = DataLoader(dataset_partition['dev'], batch_size=1)
 test_dataloader = DataLoader(dataset_partition['test'], batch_size=1)
-lr = 1e-3
-optim_scheduler_warmup_steps = 1
-optim_step_every = 1
-optim_max_grad_norm = 5.0
 
 # Model
 char_emb = nn.Embedding.from_pretrained(torch.tensor(char_vectors, dtype=torch.float),
                                         freeze=False, padding_idx=char2index['<pad>'])
-token_morph_seg_model = TokenSeq2SeqMorphSeg(char_emb, enc_hidden_size=bert.config.hidden_size, enc_num_layers=1,
-                                             enc_dropout=0.0, dec_num_layers=1, dec_dropout=0.0,
-                                             out_size=len(char2index))
-seg_model = MorphSegModel(bert, bert_tokenizer, char_emb, char_vocab, token_morph_seg_model)
-# freeze all the parameters
-for param in bert.parameters():
-    param.requires_grad = False
-parameters = list(filter(lambda p: p.requires_grad, seg_model.parameters()))
-# parameters = seg_model.parameters()
-
-# Optimization
-adam = AdamW(parameters, lr=lr)
-# lr_scheduler = get_linear_schedule_with_warmup(adam, num_warmup_steps=optim_scheduler_warmup_steps,
-#                                                num_training_steps=total_train_steps)
-# optimizer = ModelOptimizer(parameters, optimizer, optim_step_every, optim_max_grad_norm, lr_scheduler)
+num_layers = 2
+hidden_size = bert.config.hidden_size // num_layers
+enc_dropout = 0.1
+dec_dropout = 0.1
+out_size = len(char2index)
+out_dropout = 0.3
+token_morph_seg_model = TokenSeq2SeqMorphSeg(char_emb, hidden_size=hidden_size, num_layers=num_layers,
+                                             enc_dropout=enc_dropout, dec_dropout=dec_dropout, out_size=out_size,
+                                             out_dropout=out_dropout)
+seg_model = MorphSegModel(bert, bert_tokenizer, char_emb, token_morph_seg_model)
 
 
 def to_token_chars(chars, char_vocab):
@@ -139,7 +130,7 @@ def print_form_sample(df):
     print(' '.join(forms))
 
 
-def decode(input_labels, num_tokens, max_form_len, char_vocab):
+def pack_padded_labels(input_labels, num_tokens, max_form_len, char_vocab):
     input_labels = input_labels[:, :num_tokens * max_form_len]
     is_eos = True
     eos_char = char_vocab['char2index']['</s>']
@@ -161,51 +152,67 @@ def decode(input_labels, num_tokens, max_form_len, char_vocab):
     return labels
 
 
-def process(epoch, phase, print_every, model, data, raw_truth_data, teacher_forcing_ratio, optimizer=None):
+def process(epoch, phase, print_every, model, data, target_raw_data, teacher_forcing_ratio, criterion, optimizer=None,
+            scheduler=None, max_grad_norm=None):
     print_loss, total_loss = 0, 0
+    print_acc, total_acc = 0, 0
     print_data, total_data = [], []
-    sos_char = torch.tensor([char2index['<s>']], dtype=torch.long, device=device)
-    eos_char = torch.tensor([char2index['</s>']], dtype=torch.long, device=device)
+    sos = torch.tensor([char2index['<s>']], dtype=torch.long, device=device)
+    eos = torch.tensor([char2index['</s>']], dtype=torch.long, device=device)
     for i, batch in enumerate(data):
         batch = tuple(t.to(device) for t in batch)
-        xtoken_data, token_char_data, form_char_data, token_form_char_data = batch
+        xtoken_data, token_char_data, _, token_form_char_data = batch
         xtokens = xtoken_data[:, :, 1]
         sent_ids = token_char_data[:, :, 0]
-        token_chars = token_char_data[:, :, 1:]
-        token_form_chars = token_form_char_data[:, :, 1]
-        max_num_tokens = token_form_char_data[:, :, 2].unique().item()
+        input_token_chars = token_char_data[:, :, 1:]
+        target_chars = token_form_char_data[:, :, 1]
+        # max_num_tokens = token_form_char_data[:, :, 2].unique().item()
         max_form_len = token_form_char_data[:, :, 3].unique().item()
         use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
-        num_tokens, decoded_char_scores = model(xtokens, token_chars, token_form_chars, max_num_tokens, max_form_len,
-                                                sos_char, eos_char, use_teacher_forcing)
-        batch_loss = model.loss(decoded_char_scores, token_form_chars)
+        if use_teacher_forcing:
+            num_tokens, char_scores = model(xtokens, input_token_chars, sos, eos, max_form_len, target_chars)
+        else:
+            num_tokens, char_scores = model(xtokens, input_token_chars, sos, eos, max_form_len)
+        target_chars = target_chars[:, :num_tokens * max_form_len]
+        batch_loss = criterion(char_scores[0], target_chars[0])
         print_loss += batch_loss
         if optimizer is not None:
             batch_loss.backward()
+            # Gradient clipping
+            if max_grad_norm:
+                torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
             optimizer.step()
+            scheduler.step()
             optimizer.zero_grad()
         # gold_chars = decode(token_form_chars, num_tokens, max_form_len, model.char_vocab)
         # gold_form_chars = to_token_chars(gold_chars, model.char_vocab)
-        decoded_chars = model.decode(decoded_char_scores)
-        decoded_chars = decode(decoded_chars, num_tokens, max_form_len, model.char_vocab)
-        decoded_form_chars = to_token_chars(decoded_chars, model.char_vocab)
-        token_chars = token_chars[:, :, [0, 2]]
-        print_data.append((sent_ids.cpu().numpy(), token_chars.cpu().numpy(), decoded_form_chars.cpu().numpy()))
+        predicted_chars = model.decode(char_scores)
+        predicted_chars = pack_padded_labels(predicted_chars, num_tokens, max_form_len, char_vocab)
+        # target_chars = pack_padded_labels(target_chars, num_tokens, max_form_len, char_vocab)
+        # acc = accuracy_score(predicted_chars, target_chars)
+        # print_acc += acc
+        predicted_form_chars = to_token_chars(predicted_chars, char_vocab)
+        token_chars = input_token_chars[:, :, [0, 2]]
+        print_data.append((sent_ids.cpu().numpy(), token_chars.cpu().numpy(), predicted_form_chars.cpu().numpy()))
         if (i + 1) % print_every == 0:
             print(f'epoch {epoch} {phase}, step {i + 1} loss: {print_loss / len(print_data)}')
-            raw_decoded_data = dataset.to_raw_data(print_data, model.char_vocab)
-            print_eval_scores(raw_truth_data, raw_decoded_data, i+1)
-            print_form_sample(raw_decoded_data)
+            print(f'optimizer.lr = {optimizer.param_groups[0]["lr"]}')
+            # print(f'epoch {epoch} {phase}, step {i + 1} acc: {print_acc / len(print_data)}')
+            predicted_raw_data = dataset.to_raw_data(print_data, char_vocab)
+            print_eval_scores(target_raw_data, predicted_raw_data, i+1)
+            print_form_sample(predicted_raw_data)
             total_loss += print_loss
+            total_acc += print_acc
             total_data.extend(print_data)
-            print_loss, print_data = 0, []
+            print_loss, print_acc, print_data = 0, 0, []
     if len(print_data) > 0:
         total_loss += print_loss
         total_data.extend(print_data)
     print(f'epoch {epoch} {phase}, total loss: {total_loss / len(total_data)}')
-    raw_decoded_data = dataset.to_raw_data(total_data, model.char_vocab)
-    print_eval_scores(raw_truth_data, raw_decoded_data, 'total')
-    return raw_decoded_data
+    # print(f'epoch {epoch} {phase}, total acc: {total_acc / len(total_data)}')
+    predicted_raw_data = dataset.to_raw_data(total_data, char_vocab)
+    print_eval_scores(target_raw_data, predicted_raw_data, 'total')
+    return predicted_raw_data
 
 
 raw_train_data = get_raw_data(train_dataloader, char_vocab)
@@ -215,13 +222,35 @@ device = None
 if device is not None:
     seg_model.to(device)
 print(seg_model)
-teacher_forcing_ratio = 1.0
+
+
 epochs = 3
-# total_train_steps = (len(train_dataloader.dataset) // train_batch_size // optim_step_every // epochs)
+lr = 1e-3
+scheduler_warmup_steps = 200
+step_every = 1
+max_grad_norm = 5.0
+num_training_batchs = len(train_dataloader.dataset) // train_batch_size
+num_training_steps = num_training_batchs * epochs
+
+# Optimization
+# freeze bert
+for param in bert.parameters():
+    param.requires_grad = False
+parameters = list(filter(lambda p: p.requires_grad, seg_model.parameters()))
+# parameters = seg_model.parameters()
+adam = AdamW(parameters, lr=lr)
+loss_fct = nn.CrossEntropyLoss(reduction='mean', ignore_index=char_vocab['char2index']['<pad>'])
+
+lr_scheduler = get_linear_schedule_with_warmup(adam, num_warmup_steps=scheduler_warmup_steps,
+                                               num_training_steps=num_training_steps)
+# optimizer = ModelOptimizer(parameters, optimizer, optim_step_every, optim_max_grad_norm, lr_scheduler)
+
+teacher_forcing_ratio = 1.0
 for i in trange(epochs, desc="Epoch"):
     epoch = i + 1
     seg_model.train()
-    process(epoch, 'train', 10, seg_model, train_dataloader, raw_train_data, teacher_forcing_ratio, adam)
+    process(epoch, 'train', 10, seg_model, train_dataloader, raw_train_data, teacher_forcing_ratio, loss_fct, adam,
+            lr_scheduler, max_grad_norm)
     seg_model.eval()
     with torch.no_grad():
-        samples = process(epoch, 'dev', 10, seg_model, dev_dataloader, raw_dev_data, 0.0)
+        samples = process(epoch, 'dev', 10, seg_model, dev_dataloader, raw_dev_data, 0.0, loss_fct)
